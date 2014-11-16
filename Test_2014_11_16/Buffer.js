100ae28dc89d996a146b4fe54c8b0e7910bc8c96
wanghytosky <wanghytosky@gmail.com>
2014-11-16 17:26:44 +0800

    buffer练习

---
+++ Test_2014_11_16/Buffer.js
@@ -0,0 +1,16 @@
+﻿var str="王涵杨联系node.js";
+var buf=new Buffer(str,'utf-8');
+console.log(buf);
+//<Buffer e7 8e 8b e6 b6 b5 e6 9d a8 e8 81 94 e7 b3 bb 6e 6f 64 65 2e 6a 73>
+
+var buf=new Buffer(100);
+console.log(buf.length);
+
+console.log(buf[10]);
+
+buf[20]=-100;
+console.log(buf[20]);
+buf[30]=300;
+console.log(buf[30]);
+buf[40]=3.1415;
+console.log(buf[40]);
