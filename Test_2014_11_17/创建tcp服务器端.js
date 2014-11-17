ce79785e03ecaf7486cf332966ced243c78890d3
wanghytosky <wanghytosky@gmail.com>
2014-11-17 22:57:45 +0800

    tcp服务器练习

---
+++ Test_2014_11_17/创建tcp服务器端.js
@@ -0,0 +1,22 @@
+﻿var net=require('net');
+var server=net.createServer(function(socket)
+{
+	socket.on('data',function(data)
+	{
+		console.log('i am coming');
+		socket.write('hello:'+data);
+	});
+	
+	socket.on('end',function()
+	{
+		console.log('end');
+	});
+	
+	socket.write("welcome!");
+});
+
+server.listen(8124,function()
+{
+	console.log('server bound');
+});
+
