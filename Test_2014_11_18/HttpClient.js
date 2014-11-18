ef9758246bfb201413d0f652f1bcedf775203ea2
wanghytosky <wanghytosky@gmail.com>
2014-11-18 23:33:47 +0800

    http服务&客户端

---
+++ Test_2014_11_18/HttpClient.js
@@ -0,0 +1,23 @@
+﻿var options=
+{
+	hostname:'127.0.0.1',
+	port:8887,
+	path:'/',
+	method:'GET'
+};
+var http=require('http');
+var req=http.request(options,function(res)
+{
+	var i=0;
+	console.log('STATUS:'+res.statusCode);
+	console.log('HEADERS'+JSON.stringify(res.headers));
+	res.setEncoding('utf8');
+	res.on('data',function(chunk)
+	{
+		i++;
+		console.log(chunk+'A');
+		console.log(i);
+	});
+});
+
+req.end();
