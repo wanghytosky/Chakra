550b82546063c24d8201de91e1d64511ffbb5cb4
wanghytosky <wanghytosky@gmail.com>
2014-11-23 15:08:41 +0800

    写入mongodb

---
+++ Test_2014_11_23/MongoRequest.js
@@ -0,0 +1,22 @@
+﻿var http=require('http');
+var options=
+{
+	hostname:'127.0.0.1',
+	port:8887,
+	path:'/',
+	method:'Get'
+};
+var jsonStr={hostname:'127.0.0.1',info:'测试mongo请求,时间:'+new Date()};
+var req=http.request(options,function(err,res)
+{
+	console.log(res.statusCode);
+	res.on('error',function(err)
+	{
+		console.log('请求异常:'+JSON.stringify(err));
+	});
+});
+req.end(function()
+{
+	console.log('请求结束');
+});
+
---
+++ Test_2014_11_23/Mongoose.js
@@ -0,0 +1,42 @@
+﻿var http=require('http');
+var mongoose=require('mongoose');
+var db=mongoose.createConnection('mongodb://localhost:27017/MonitorLog');
+
+var monitorSchema=new mongoose.Schema(
+{
+	hostname:String,
+	info:String
+});
+
+http.createServer(function(req,res)
+{
+	insertIntoMongoDB(req,res);
+}).listen(8887,'127.0.0.1');
+
+db.on('error',function(error)
+{
+	console.log(error);
+	mongoose.connection.close(function()
+	{
+		console.log('mongdb连接关闭');
+	});
+});
+db.once('open',function()
+{
+	//打开记录
+	console.log('mongoDB被打开');
+});
+var jsonStr={hostname:'127.0.0.1',info:'测试mongo请求,时间:'+new Date()};
+function insertIntoMongoDB(req,res)
+{
+	try
+	{
+		var PersonModel=db.model('MonitorLog',monitorSchema);
+		var PersonEntity=new PersonModel({hostname:jsonStr.hostname,info:jsonStr.info});
+		PersonEntity.save();		
+	}
+	catch(err)
+	{
+		console.log('出现异常:'+err);
+	}
+}
