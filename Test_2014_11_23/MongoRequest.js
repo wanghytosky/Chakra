7a367b4dacef43d5ce73eeab9984085a6f9ec238
wanghytosky <wanghytosky@gmail.com>
2014-11-23 21:39:29 +0800

    mongo request test

--- Test_2014_11_23/MongoRequest.js
+++ Test_2014_11_23/MongoRequest.js
@@ -1,13 +1,13 @@
 ﻿var http=require('http');
 var options=
 {
-	hostname:'127.0.0.1',
+	hostname:'192.168.199.104',
 	port:8887,
 	path:'/',
-	method:'Get'
+	method:'POST'
 };
-var jsonStr={hostname:'127.0.0.1',info:'测试mongo请求,时间:'+new Date()};
-var req=http.request(options,function(err,res)
+var jsonStr={hostname:'127.0.0.1',info:'测试mongo请求,时间'+new Date()};
+var req=http.request(options,function(res)
 {
 	console.log(res.statusCode);
 	res.on('error',function(err)
@@ -15,8 +15,6 @@ var req=http.request(options,function(err,res)
 		console.log('请求异常:'+JSON.stringify(err));
 	});
 });
-req.end(function()
-{
-	console.log('请求结束');
-});
+req.write(JSON.stringify(jsonStr));
+req.end();
 
--- Test_2014_11_23/Mongoose.js
+++ Test_2014_11_23/Mongoose.js
@@ -1,6 +1,7 @@
 ﻿var http=require('http');
 var mongoose=require('mongoose');
 var db=mongoose.createConnection('mongodb://localhost:27017/MonitorLog');
+var querystring = require("querystring");
 
 var monitorSchema=new mongoose.Schema(
 {
@@ -11,7 +12,7 @@ var monitorSchema=new mongoose.Schema(
 http.createServer(function(req,res)
 {
 	insertIntoMongoDB(req,res);
-}).listen(8887,'127.0.0.1');
+}).listen(8887,'192.168.199.104');
 
 db.on('error',function(error)
 {
@@ -26,14 +27,34 @@ db.once('open',function()
 	//打开记录
 	console.log('mongoDB被打开');
 });
-var jsonStr={hostname:'127.0.0.1',info:'测试mongo请求,时间:'+new Date()};
+//var jsonStr={hostname:'127.0.0.1',info:'测试mongo请求,时间:'+new Date()};
 function insertIntoMongoDB(req,res)
 {
 	try
 	{
-		var PersonModel=db.model('MonitorLog',monitorSchema);
-		var PersonEntity=new PersonModel({hostname:jsonStr.hostname,info:jsonStr.info});
-		PersonEntity.save();		
+		var data='';
+		var jsonStr={};
+		//console.log(JSON.stringify(req.headers));
+		//console.log(JSON.stringify(req));
+		req.setEncoding("utf8");
+		req.on('data',function(chunk)
+		{
+			console.log(chunk);
+			data+=chunk;
+			console.log(data);
+		}).on('end',function()
+		{
+			console.log('接收完成');
+			jsonStr=JSON.parse(data);
+			console.log(jsonStr.hostname);
+			console.log(jsonStr.info);
+			var PersonModel=db.model('MonitorLog',monitorSchema);
+			var PersonEntity=new PersonModel({hostname:jsonStr.hostname,info:jsonStr.info});
+			PersonEntity.save();
+			res.writeHead(200,{'Content-Type':'text/plain'});
+			res.end('Hello World!');
+		});
+		//var objectPostData = querystring.parse(data);
 	}
 	catch(err)
 	{
