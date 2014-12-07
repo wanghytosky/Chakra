7b0f841c313af74ff0b7a272c8adaae3bd38a9f1
wanghytosky <wanghytosky@gmail.com>
2014-12-07 10:38:55 +0800

    浮点数场宽格式化

---
+++ C_Test/Test_2014_12_07/FloatFormat.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+#include <string.h>
+
+
+int main(int argc, const char * argv[]) {
+    float pi=30000.1415926;
+    printf("%2.3f\n",pi);
+    printf("%3.3f\n",pi);
+    printf("%4.3f\n",pi);
+    printf("%50.3f\n",pi);
+    printf("%10.3f\n",pi);
+}
